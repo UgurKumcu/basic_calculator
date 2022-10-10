eb7c8355d392fe01d7e8ba1156398f3cfb2299e4
UgurKumcu <ugur.kumcu@boun.edu.tr>
2022-10-11 00:28:12 +0300

    Basic calculator coding using only JS

---
+++ assets/scripts/app.js
@@ -0,0 +1,71 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserInputNumber() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+}
+
+function calculateResult(calculationType) {
+  const enteredNumber = getUserInputNumber();
+  const initialResult = currentResult;
+  let mathOperator;
+
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculationType == "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculationType == "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculationType == "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  } else {
+    return;
+  }
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+  calculateResult("ADD");
+}
+
+function subtract() {
+  calculateResult("SUBTRACT");
+}
+
+function multiply() {
+  calculateResult("MULTIPLY");
+}
+
+function divide() {
+  calculateResult("DIVIDE");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
